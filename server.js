a9a26ddab1c7abf27c756550e255b0c899573aba
Kumar Shrey <kumarshrey.21099@gmail.com>
2021-02-07 23:47:39 +0530

    misc commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose
     .catch(err=>console.log(err));
 // ********************************************
 
-app.get('/', (req,res)=>res.send('Hello World!'));
+
 
 //use routes
 app.use('/memes',users);
