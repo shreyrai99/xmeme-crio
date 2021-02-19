8d424101988d7b79d2068b11e751074fb6444f1b
Kumar Shrey <kumarshrey.21099@gmail.com>
2021-02-19 13:34:37 +0530

    removed spelling errors

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-sudo apt-get install -y nodejs 
+#!/bin/bash
+
+sudo apt-get install -y nodejs
+
+sudo apt install -y npm
 
 curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-
 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
@@ -9,13 +12,8 @@ sudo apt update
 
 sudo apt-get install -y mongodb-org
 
-sudo systemct1 start mongod.service
+sudo systemctl start mongod.service
 
-sudo systemct1 status mongod
+sudo systemctl status mongod
 
-sudo systemct1 enable mongod 
-
-
-
-
-
+sudo systemctl enable mongod
